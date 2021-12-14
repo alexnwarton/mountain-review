94dff4ca5f0c49de281c6c8195711d9ec45a2009
Alex Warton <alexnwarton@gmail.com>
2021-12-14 10:21:01 -0800

    commit for initial deployment

--- client/src/screens/HomePage.js
+++ client/src/screens/HomePage.js
@@ -1,11 +1,9 @@
-//import { Link } from 'react-router-dom';
 import '../assets/css/HomePage.css';
 
 const HomePage = () => {
 	return (
 		<div className='home-page'>
 			<h1>Mountain Review</h1>
-			{/*<Link to='/resorts'>View all resorts</Link>*/}
 		</div>
 	)
 }
